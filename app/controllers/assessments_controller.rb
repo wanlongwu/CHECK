0362a5438dda7bd817bd5526dbf8a26280014c11
Wanlong Wu <wumalone@gmail.com>
2018-03-08 21:11:11 +0900

    wip

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -5,7 +5,9 @@ class AssessmentsController < ApplicationController
     @assessment = Assessment.new
   end
   def create
+    change_img_params(assessment_params[:"photo"])
     @assessment = Assessment.new(assessment_params)
+    # @photo =
     @assessment.user = current_or_guest_user
     @assessment.save!
     @exercise = Exercise.all.sample
@@ -23,5 +25,32 @@ class AssessmentsController < ApplicationController
 
   def assessment_params
     params.require(:assessment).permit(:angle1,:angle2,:angle3,:photo)
+  end
+
+  def change_img_params(img)
+    begin
+    Base64.decode64(img) #To check if thats a base64 string
+      if img
+        img = file_decode(img.split(',')[1],"some file name") #getting only the string leaving out the data/<format>
+      end
+      rescue Exception => e
+      img #Returning if its not a base64 string
+      end
+  end
+
+  def file_decode(base, filename)
+    file = Tempfile.new([file_base_name(filename), file_extn_name(filename)])
+    file.binmode
+    file.write(Base64.decode64(base))
+    file.close
+    file
+  end
+
+  def file_base_name(file_name)
+    File.basename(file_name, file_extn_name(file_name))
+  end
+
+  def file_extn_name(file_name)
+    File.extname(file_name)
   end
 end
