b7c1aac8b4b1a0d71f558fdc790b2d0418a88a31
Wanlong Wu <wumalone@gmail.com>
2018-03-10 11:14:54 +0900

    basic notification method tested working

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -1,5 +1,31 @@
 // const hello = () => {
-  console.log("hello")
+  // console.log("hello")
 // };
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.');
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Notification title', {
+      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: "Hey there! You've been notified!",
+    });
+
+    notification.onclick = function () {
+      window.open("http://stackoverflow.com/a/13328397/1269037");
+    };
+
+  }
+
+}
