d73c5f740661e654b6f39addaf60e7a05e1e1d38
Wanlong Wu <wumalone@gmail.com>
2018-03-15 11:19:45 +0900

    profile drop down fixed, youtube video with seeding fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,42 +27,42 @@ end
 
   Exercise.create([{
     name:"FIX Forward Head Posture! (Daily Corrective Routine)",
-    content:"https://www.youtube.com/embed/wQylqaCl8Zo",
+    content:"https://www.youtube.com/embed/wQylqaCl8Zo?enablejsapi=1&amp;autoplay=0",
     category:"neck"
   },
   {
     name:"4 stretches that get rid of neck pain and give you better posture",
-    content:"https://www.youtube.com/embed/NaShux69R1w",
+    content:"https://www.youtube.com/embed/NaShux69R1w?enablejsapi=1&amp;autoplay=0",
     category:"neck"
   },
   {
     name:"Corrective Exercise Forward Head Posture",
-    content:"https://www.youtube.com/embed/JA3O0NVb-sk",
+    content:"https://www.youtube.com/embed/JA3O0NVb-sk?enablejsapi=1&amp;autoplay=0",
     category:"neck"
   },
   {
     name:"how to Get Rid of a Neck Hump",
-    content:"https://www.youtube.com/embed/0OkB0xO_9kU",
+    content:"https://www.youtube.com/embed/0OkB0xO_9kU?enablejsapi=1&amp;autoplay=0",
     category:"neck"
   },
   {
     name: "5 Exercises To Fix Hunchback Posture",
-    content:"https://www.youtube.com/embed/wr1ONXcq3yM",
+    content:"https://www.youtube.com/embed/wr1ONXcq3yM?enablejsapi=1&amp;autoplay=0",
     category:"back"
   },
   {
     name:"Improve Your Posture | 3 Exercises Only!",
-    content:"https://www.youtube.com/embed/CqTtScFQ5-c",
+    content:"https://www.youtube.com/embed/CqTtScFQ5-c?enablejsapi=1&amp;autoplay=0",
     category:"back"
   },
   {
     name:"How To Fix Uneven Hips",
-    content:"https://www.youtube.com/embed/lhbhay5_M-0",
+    content:"https://www.youtube.com/embed/lhbhay5_M-0?enablejsapi=1&amp;autoplay=0",
     category:"back"
   },
   {
    name:"Exercise to Fix Posture and Open Your Hips",
-    content:"https://www.youtube.com/embed/4LZ5Bv8ZV9s",
+    content:"https://www.youtube.com/embed/4LZ5Bv8ZV9s?enablejsapi=1&amp;autoplay=0",
     category:"back"
   },
   ])
